95a7b09d4b7224ccda8c0d9b6d3d5e2ab6752a0b
itsmaxymoo <max.loiacono@protonmail.com>
2022-11-01 22:43:57 -0400

    add pizza card html

---
+++ src/AddPizzaCard.svelte
@@ -0,0 +1,35 @@
+<script>
+
+</script>
+
+<div class="box">
+
+    <p class="has-text-centered mb-3">
+        <span class="is-size-3">
+            Add Pizza
+        </span>
+    </p>
+
+    <div class="field is-grouped-centered">
+        <div class="field-body">
+
+            <div class="field has-addons">
+                <p class="control has-icons-right">
+                    <input class="input" type="number" placeholder="size" min="0" required/>
+                    <span class="icon is-right has-text-grey-dark">"</span>
+                </p>
+
+                <p class="control has-icons-left">
+                    <span class="icon is-left has-text-grey-dark">$</span>
+                    <input class="input" type="number" placeholder="price" min="0" required/>
+                </p>
+
+                <p class="control">
+                    <button class="button is-info">+</button>
+                </p>
+            </div>
+
+        </div>
+    </div>
+
+</div>
