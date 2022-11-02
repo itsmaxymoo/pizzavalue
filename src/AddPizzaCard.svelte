4ad3c2fc78590f7ad84cdd3884f69e584ed26ec3
itsmaxymoo <max.loiacono@protonmail.com>
2022-11-01 23:25:23 -0400

    hide number up/down selectors

--- src/AddPizzaCard.svelte
+++ src/AddPizzaCard.svelte
@@ -33,3 +33,18 @@
     </div>
 
 </div>
+
+<style>
+    /* Hide number up/down selector */
+    /* Chrome, Safari, Edge, Opera */
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+
+    /* Firefox */
+    input[type=number] {
+        -moz-appearance: textfield;
+    }
+</style>
