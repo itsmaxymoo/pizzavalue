881b0130384974960df8732c5fada051c4240676
itsmaxymoo <max.loiacono@protonmail.com>
2022-11-03 16:24:16 -0400

    delete all pizzas button

--- src/MainView.svelte
+++ src/MainView.svelte
@@ -3,6 +3,7 @@
     import TopBar from "./TopBar.svelte";
     import PizzaListView from "./PizzaListView.svelte";
     import AddPizzaCard from "./AddPizzaCard.svelte";
+    import DeleteAllButton from "./RemoveAllButton.svelte";
 
     let pizzaList = [];
 
@@ -10,11 +11,17 @@
         pizzaList = [...pizzaList, event.detail.pizza];
     }
 
+    function onDeleteAll() {
+        pizzaList = [];
+    }
+
 </script>
+
 
 <TopBar/>
 
 <section class="section container">
     <PizzaListView pizzaList="{pizzaList}"/>
     <AddPizzaCard on:pizza={onAddPizza}/>
+    <DeleteAllButton on:click={onDeleteAll}/>
 </section>
---
+++ src/RemoveAllButton.svelte
@@ -0,0 +1,7 @@
+<div class="has-text-centered pt-4">
+    <button
+            class="button is-danger is-outlined"
+            on:click>
+        remove all
+    </button>
+</div>
