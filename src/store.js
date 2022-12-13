78f61bcee25f40631a3cb6707a5ec3d7148ca1fd
itsmaxymoo <max.loiacono@protonmail.com>
2022-12-12 21:21:35 -0500

    pizzas persist

---
+++ src/store.js
@@ -0,0 +1,33 @@
+import { writable } from "svelte/store";
+import { Pizza } from "./pizza";
+
+
+const pizzaValueKey = "pizzaValue";
+
+
+function _loadPizzaList() {
+	try {
+		// Try to load and parse pizzaList
+		let ls = localStorage.getItem(pizzaValueKey)
+		let parsed = JSON.parse(ls);
+
+		// Throw parsing error if relevant - return empty array
+		if (!parsed || !Array.isArray(parsed)) throw new TypeError;
+
+		// Convert regular json array to array of pizzas
+		for (let i = 0; i < parsed.length; i++) {
+			parsed[i] = Pizza.fromJSON(parsed[i]);
+		}
+
+		return parsed;
+	}
+	catch {
+		return [];
+	}
+}
+
+
+export const pizzaList = writable(_loadPizzaList());
+pizzaList.subscribe(value => {
+	localStorage.setItem(pizzaValueKey, JSON.stringify(value));
+});
